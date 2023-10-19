9505774d11ec76172c575d9d0ec4c6b565fa8157
Ahmedismaeell <86728394+Ahmedismaeell@users.noreply.github.com>
2023-10-19 14:37:29 +0400

    change to yml

--- src/main/java/com/mystoreapp/storeapp/test/AdminApi.java
+++ src/main/java/com/mystoreapp/storeapp/test/AdminApi.java
@@ -3,6 +3,7 @@ package com.mystoreapp.storeapp.test;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/admin")
+@PreAuthorize("hasAnyRole('ADMIN')")
 public class AdminApi {
 
     @GetMapping("/test")
+    @PreAuthorize("hasAuthority('admin:read')")
     ResponseEntity<?> testAdminApi(){
         return ResponseEntity.ok().body("Welcome to Admin Api");
     }
